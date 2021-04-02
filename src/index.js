d1c2526b0e5ceea46160c01ee079ac0b235f6234
Tsuyoshi.m <tsuyoshi.m.it@gmail.com>
2021-04-02 12:58:24 +0900

    try arr

--- src/index.js
+++ src/index.js
@@ -124,3 +124,42 @@
 // console.log(message4);
 
 //------------------------------------------------
+/**
+ * デフォルト値、引数
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello();
+
+/**
+ * スプレッド構文
+ */
+//配列の展開
+// const arr1 = [1,2];
+// console.log(arr1);
+// console.log(...arr1);//配列だったものが格納された値のみになっている
+
+// const sumFunc1 = (num1,num2) => console.log(num1 + num2);
+// sumFunc1(arr1[0],arr1[1]);
+// sumFunc1(...arr1);
+
+//配列をまとめる
+// const arr2 = [1,2,3,4,5];
+// const [num1,num2,...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー・結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4,...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);//arr4にも影響が出てしまう。
